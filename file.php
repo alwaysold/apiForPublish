93394d4e708f1baf00dc4729306f28f6f96afd6b
Amit Anand <dropmail.amit@gmail.com>
2023-11-26 17:34:41 +0530

    sdfs

---
+++ file.php
@@ -0,0 +1,15 @@
+<?php
+
+$directoryPath = '/path/to/your/directory';
+$command = 'chmod 755 ' . $directoryPath;
+
+$output = shell_exec($command);
+
+// Checking the output can help verify if the command executed successfully
+if ($output === null) {
+    echo "Permissions changed successfully for: $directoryPath";
+} else {
+    echo "Failed to change permissions for: $directoryPath";
+    // Log the error if needed
+    error_log("Failed to change permissions for: $directoryPath");
+}
