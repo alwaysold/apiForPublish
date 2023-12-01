f001372826a8854ef53483f2853e648838819ebf
Amit Anand <dropmail.amit@gmail.com>
2023-12-01 10:16:16 +0530

    gdfs

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -12,7 +12,7 @@ class Feed extends Database
 
   public function list($num)
   {
-      echo $num;
+    //   echo $num;
     try {
       $stm = $this->pdo->prepare("SELECT `title`, `content`, `media_url`, `abstract`, `author_id`, `publication_date` FROM `Papers` LIMIT ?");
       $stm->execute([$num]);
