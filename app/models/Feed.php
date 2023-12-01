fb3001968721a65747c02141ddd2cddecdaa9429
Amit Anand <dropmail.amit@gmail.com>
2023-12-01 10:22:17 +0530

    sdfg

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -12,18 +12,17 @@ class Feed extends Database
 
   public function list($id)
   {
-    //   echo $num;
-    // try {
-      $stm = $this->pdo->prepare("SELECT `title`, `content`, `media_url`, `abstract`, `author_id`, `publication_date` FROM `Papers` LIMIT ?");
-      $stm->execute([$id]);
+    try {
+      $stm = $this->pdo->prepare("SELECT `title`, `content`, `media_url`, `abstract`, `author_id`, `publication_date` FROM `Papers` LIMIT $id ");
+      $stm->execute();
     var_dump($stm);
       if ($stm->rowCount() > 0) {
         return $stm->fetch(PDO::FETCH_ASSOC);
       } else {
         return false;
       }
-    // } catch (PDOException $err) {
-    //   return false;
-    // }
+    } catch (PDOException $err) {
+      return false;
+    }
   }
 }
