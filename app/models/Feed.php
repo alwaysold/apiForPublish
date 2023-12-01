5dd32b6aa91b465605827758350f10b01a89df07
Amit Anand <dropmail.amit@gmail.com>
2023-12-01 10:22:45 +0530

    hjg

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -15,7 +15,7 @@ class Feed extends Database
     try {
       $stm = $this->pdo->prepare("SELECT `title`, `content`, `media_url`, `abstract`, `author_id`, `publication_date` FROM `Papers` LIMIT $id ");
       $stm->execute();
-    var_dump($stm);
+    // var_dump($stm);
       if ($stm->rowCount() > 0) {
         return $stm->fetch(PDO::FETCH_ASSOC);
       } else {
