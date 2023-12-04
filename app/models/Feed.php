8b4e2daf5dccf61896cbebd80cb9bc3a240835b5
Amit Anand <dropmail.amit@gmail.com>
2023-12-04 10:30:10 +0530

    sfd

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -13,7 +13,7 @@ class Feed extends Database
     public function list($id)
     {
         try {
-            $stm = $this->pdo->prepare("SELECT `title`, `content`, `media_url`, `abstract`, `author_id`, `publication_date` FROM `Papers` LIMIT $id ");
+            $stm = $this->pdo->prepare("SELECT `paper_id`, `title`, `content`, `media_url`, `abstract`, `author_id`, `publication_date` FROM `Papers` LIMIT $id ");
             $stm->execute();
             // var_dump($stm);
             if ($stm->rowCount() > 0) {
