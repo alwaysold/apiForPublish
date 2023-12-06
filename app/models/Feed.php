eb6980c769c4740d35abb44c171f101abfd7ebae
Amit Anand <dropmail.amit@gmail.com>
2023-12-06 10:24:24 +0530

    sdf

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -16,35 +16,35 @@ class Feed extends Database
         try {
             $stm = $this->pdo->prepare("
             SELECT 
-    p.paper_id, 
-    p.title, 
-    p.content, 
-    p.media_url, 
-    p.abstract, 
-    p.author_id, 
-    p.publication_date,
-    COALESCE(reactions.like, 0) AS like,
-    COALESCE(reactions.dislike, 0) AS dislike,
-    COALESCE(reactions.insightfull, 0) AS insightfull,
-    COALESCE(reactions.celebrate, 0) AS celebrate,
-    COALESCE(reactions.support, 0) AS support
-FROM 
-    Papers p
-LEFT JOIN 
-    (
-        SELECT 
-            paper_id,
-            SUM(reaction_type = 1) AS like,
-            SUM(reaction_type = 2) AS dislike,
-            SUM(reaction_type = 3) AS insightfull,
-            SUM(reaction_type = 4) AS celebrate,
-            SUM(reaction_type = 5) AS support
-        FROM 
-            Reactions
-        GROUP BY 
-            paper_id
-    ) reactions ON p.paper_id = reactions.paper_id
-LIMIT $id
+                p.paper_id, 
+                p.title, 
+                p.content, 
+                p.media_url, 
+                p.abstract, 
+                p.author_id, 
+                p.publication_date,
+                COALESCE(reactions.count_type_1, 0) AS count_type_1,
+                COALESCE(reactions.count_type_2, 0) AS count_type_2,
+                COALESCE(reactions.count_type_3, 0) AS count_type_3,
+                COALESCE(reactions.count_type_4, 0) AS count_type_4,
+                COALESCE(reactions.count_type_5, 0) AS count_type_5
+            FROM 
+                Papers p
+            LEFT JOIN 
+                (
+                    SELECT 
+                        paper_id,
+                        SUM(reaction_type = 1) AS like,
+                        SUM(reaction_type = 2) AS count_type_2,
+                        SUM(reaction_type = 3) AS count_type_3,
+                        SUM(reaction_type = 4) AS count_type_4,
+                        SUM(reaction_type = 5) AS count_type_5
+                    FROM 
+                        Reactions
+                    GROUP BY 
+                        paper_id
+                ) reactions ON p.paper_id = reactions.paper_id
+            LIMIT $id
 
             ");
             $stm->execute();
